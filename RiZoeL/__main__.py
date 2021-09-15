eb8824f9a09872c61f2155168e39dd1aa82ec751
RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-09-15 15:36:45 +0530

    Add files via upload

---
+++ RiZoeL/__main__.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from RiZoeL.VideoStreaming import app
+
+
+RiZoeL = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="RiZoeL"),
+)
+
+Misery.start()
+print("[INFO]: STARTING BOT CLIENT")
+app.start()
+print("[INFO]: STARTING USERBOT CLIENT")
+idle()
